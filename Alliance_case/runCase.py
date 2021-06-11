213503907c36e220fd9ef80314ac9a0c60485ed9
Runtao <851379672@qq.com>
2021-06-11 10:29:07 +0800

    first commit

--- Alliance_case/runCase.py
+++ Alliance_case/runCase.py
@@ -1,20 +1,20 @@
 # coding=utf-8
-from 任务构建程序 import *
-from 扫描上传程序 import *
-from 分配任务程序 import *
-from 接口评阅程序 import *
-from 成绩相关程序 import *
-from sha_ import *
+from testcase.任务构建程序 import *
+from testcase.扫描上传程序 import *
+from testcase.分配任务程序 import *
+from testcase.接口评阅程序 import *
+from testcase.成绩相关程序 import *
+from utils.sha_ import *
+from utils.def_ import *
 from BeautifulReport import BeautifulReport
 import unittest
-import threading
 import os
 scale = 100
 
 
 class AllianceCase(unittest.TestCase):
 
-    def test_0(self):
+    def setUp(self):
         """执行开始程序"""
         sleep(1)
         print("执行开始，祈祷不报错".center(scale // 2, "-"))
@@ -33,16 +33,7 @@ class AllianceCase(unittest.TestCase):
 
     def test_4(self):
         """接口评阅程序"""
-        #   线程
-        file = open('D:\PyCharm\projects\intranet\Alliance_case/user.txt', 'r', encoding='utf-8')
-        next(file)
-        with file as teachers:
-            teachers = teachers.read().split('\n')
-        for thread in teachers:
-            t = threading.Thread(target=接口评阅程序, args=(thread,))
-            t.setDaemon(True)
-            t.start()
-        t.join()
+        接口评阅程序()
 
     def test_5(self):
         """成绩相关程序"""
@@ -60,6 +51,6 @@ if __name__ == '__main__':
     suite.addTest(unittest.makeSuite(AllianceCase))
     run = BeautifulReport(suite)
     run.report(description='联盟测试报告',
-               filename=u"YJ_report" + ".html",
-               report_dir=os.path.join(os.getcwd(), "report"))
+               filename=u"BeautifulReport" + ".html",
+               report_dir=os.path.join(os.getcwd(), "Report"))
     Sha().send_email()
